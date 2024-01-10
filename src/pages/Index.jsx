5d28159df9872c93b6db53097d49caa2566c02a6
chaitanya-dange <semicd99@gmail.com>
2024-01-10 09:58:40 +0530

    added axios dependency and perform basic operation of it

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Base from '../components/Base'
 import Button from 'react-bootstrap/esm/Button'
 import { toast } from 'react-toastify';
+import axios from 'axios'
 
 
 const Index = () => {
@@ -9,6 +10,24 @@ const Index = () => {
     const showToast = () => {
         toast.success("🦄 this is working")
     }
+
+    const getData = () => {
+        axios.get("https://fakestoreapi.com/products")
+            .then( (json) =>
+            {console.log(json)
+            toast.success("data is fetched")
+            })
+            .catch((error) => {
+                toast.error("Something went wrong")
+                console.log(error);
+            })
+
+                
+                
+                    
+    }
+
+
   return (
     <div>
         <Base title='shop slowly and have fun' desc={"this is awesome and just explore and go for best product at one place.." } buttonEnable={true} to="/" >
@@ -16,6 +35,7 @@ const Index = () => {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, labore similique? Consequuntur quo, rerum cupiditate modi ea beatae laborum reprehenderit, cumque vitae ipsum enim natus sapiente cum debitis nemo nobis.</p>
 
         <Button onClick={showToast} >Tostify Checking</Button>
+        <Button onClick={getData}>Get Data from Fake API</Button>
         </Base>
     </div>
   )
