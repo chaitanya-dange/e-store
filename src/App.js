f72346b3d85774286ae8bb613ec633e36a116cb6
chaitanya-dange <semicd99@gmail.com>
2024-01-10 08:33:11 +0530

    added dependency toastify  and checked working

--- src/App.js
+++ src/App.js
@@ -2,35 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import About from './pages/About'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ContactUs from './pages/ContactUs';
 import DashBoard from './pages/user/DashBoard';
-import  Profile  from './pages/user/Profile';
+import Profile from './pages/user/Profile';
 import NavBar from './components/NavBar';
 import Services from './pages/Services';
 import Index from './pages/Index';
+import { ToastContainer} from 'react-toastify';
+
 
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <NavBar/>
+      <BrowserRouter>
+        <ToastContainer />
+        <NavBar />
         <Routes>
 
-             <Route path='/' element={<Index/>}/>
-             <Route path='/about' element={<About/>}/>
-             <Route  path='/contactus' element={<ContactUs/>}/>
-             <Route  path='/services' element={<Services/>}/>
+          <Route path='/' element={<Index />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contactus' element={<ContactUs />} />
+          <Route path='/services' element={<Services />} />
 
 
 
-             <Route path='/user' element={<DashBoard/>}>
-                <Route path='profile' element={<Profile/>}/>
-             </Route>
+          <Route path='/user' element={<DashBoard />}>
+            <Route path='profile' element={<Profile />} />
+          </Route>
 
         </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
     </>
   );
 }
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Base from '../components/Base'
+import Button from 'react-bootstrap/esm/Button'
+import { toast } from 'react-toastify';
+
 
 const Index = () => {
+
+    const showToast = () => {
+        toast.success("🦄 this is working")
+    }
   return (
     <div>
         <Base title='shop slowly and have fun' desc={"this is awesome and just explore and go for best product at one place.." } buttonEnable={true} to="/" >
         <h3>this is content of index</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, labore similique? Consequuntur quo, rerum cupiditate modi ea beatae laborum reprehenderit, cumque vitae ipsum enim natus sapiente cum debitis nemo nobis.</p>
+
+        <Button onClick={showToast} >Tostify Checking</Button>
         </Base>
     </div>
   )
